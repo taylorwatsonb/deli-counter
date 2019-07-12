20de711bf069dc3127ca2a80500e13f8e58c0fb8
Taylor Watson <taylorwatsonb@gmail.com>
2019-07-12 19:25:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function takeANumber(line, name){
   
   
   
-  return `Welcome ${name}. You are number ${line} in line.`
+  return `Welcome ${name}. You are number${line} in line.`
 }
 
 function nowServing (katzDeliLine){
